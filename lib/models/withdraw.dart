d150513336cb39d3d3083bf3021aecc2e56bd55e
umarr <umarr.developer@gmail.com>
2026-01-21 10:28:24 +0600

    added withdraw schemes

---
+++ lib/models/withdraw.dart
@@ -0,0 +1,56 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'withdraw.g.dart';
+
+@JsonSerializable()
+class WithdrawScheme {
+  WithdrawScheme({
+    required this.refKey,
+    required this.number,
+    required this.posted,
+    required this.cashRegisyerKey,
+    required this.commentary,
+    required this.documentSum,
+    required this.storeKey,
+  });
+
+  @JsonKey(name: 'Ref_Key')
+  final String refKey;
+
+  @JsonKey(name: 'Number')
+  final String number;
+
+  @JsonKey(name: 'Posted')
+  final bool posted;
+
+  @JsonKey(name: 'КассаККМ_Key')
+  final String cashRegisyerKey;
+
+  @JsonKey(name: 'Комментарий')
+  final String commentary;
+
+  @JsonKey(name: 'СуммаДокумента')
+  final double documentSum;
+
+  @JsonKey(name: 'Магазин_Key')
+  final String storeKey;
+
+  factory WithdrawScheme.fromJson(Map<String, dynamic> json) =>
+      _$WithdrawSchemeFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WithdrawSchemeToJson(this);
+}
+
+@JsonSerializable()
+class WithdrawListScheme {
+  WithdrawListScheme({required this.withdraws});
+
+  @JsonKey(name: 'value')
+  final List<WithdrawScheme> withdraws;
+
+  factory WithdrawListScheme.fromJson(
+    Map<String, dynamic> json,
+  ) => _$WithdrawListSchemeFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WithdrawListSchemeToJson(this);
+}
---
+++ lib/models/withdraw.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'withdraw.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+WithdrawScheme _$WithdrawSchemeFromJson(Map<String, dynamic> json) =>
+    WithdrawScheme(
+      refKey: json['Ref_Key'] as String,
+      number: json['Number'] as String,
+      posted: json['Posted'] as bool,
+      cashRegisyerKey: json['КассаККМ_Key'] as String,
+      commentary: json['Комментарий'] as String,
+      documentSum: (json['СуммаДокумента'] as num).toDouble(),
+      storeKey: json['Магазин_Key'] as String,
+    );
+
+Map<String, dynamic> _$WithdrawSchemeToJson(WithdrawScheme instance) =>
+    <String, dynamic>{
+      'Ref_Key': instance.refKey,
+      'Number': instance.number,
+      'Posted': instance.posted,
+      'КассаККМ_Key': instance.cashRegisyerKey,
+      'Комментарий': instance.commentary,
+      'СуммаДокумента': instance.documentSum,
+      'Магазин_Key': instance.storeKey,
+    };
+
+WithdrawListScheme _$WithdrawListSchemeFromJson(Map<String, dynamic> json) =>
+    WithdrawListScheme(
+      withdraws: (json['value'] as List<dynamic>)
+          .map((e) => WithdrawScheme.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$WithdrawListSchemeToJson(WithdrawListScheme instance) =>
+    <String, dynamic>{'value': instance.withdraws};
