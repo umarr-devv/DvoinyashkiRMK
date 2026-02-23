6abd4a00eaca93f2fde63714fff2380044427afa
umarr <umarr.developer@gmail.com>
2026-02-23 14:17:35 +0600

    update WindowService some methods

--- lib/service/window.dart
+++ lib/service/window.dart
@@ -1,6 +1,8 @@
 import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:screen_retriever/screen_retriever.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 import 'package:window_manager/window_manager.dart';
 
 class WindowService {
@@ -26,8 +28,12 @@ class WindowService {
 
   Future<void> sendData(String method, dynamic payload) async {
     if (window == null) return;
-    final channel = WindowMethodChannel('channel');
-    await channel.invokeMethod(method, payload);
+    try {
+      final channel = WindowMethodChannel('channel');
+      await channel.invokeMethod(method, payload);
+    } catch (exc, st) {
+      GetIt.I<Talker>().error(exc, st);
+    }
   }
 
   static Future<List<Display>> getDisplays() async {
