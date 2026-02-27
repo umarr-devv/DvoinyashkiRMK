5778e2e949d1a414a6c798ec8a7c5dbd1f72c9c2
umarr <umarr.developer@gmail.com>
2026-02-27 16:38:21 +0600

    added movementWarehouseRef to consts

--- lib/core/consts/consts.dart
+++ lib/core/consts/consts.dart
@@ -4,3 +4,4 @@ final String storeStructureType = 'Магазин';
 final String subdivisionStructureType = 'Подразделение';
 final String warehouseStructureDetailType = 'Склад';
 final String defaultUdsExternalId = '1075';
+final String movementWarehouseRef = '366c9859-a01b-11ee-a96e-18d6c704b66b';
